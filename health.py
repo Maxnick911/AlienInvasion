28fbea2b7f45cd4ef55be54a157ad298a2f774d4
Maxnick911 <112699811+Maxnick911@users.noreply.github.com>
2024-08-13 17:50:51 +0200

    Update health.py

--- health.py
+++ health.py
@@ -8,17 +8,14 @@ class Health:
         self.settings = ai_game.settings
         self.stats = ai_game.stats
 
-        # Завантаження зображення сердечка та отримання його прямокутника
         self.image = pygame.image.load('images\\heart.png')
         self.rect = self.image.get_rect()
 
-        # Встановлення позиції для першого сердечка у лівому верхньому куті
         self.rect.x = 20
         self.rect.y = 20
 
     def show_health(self):
         """Відображення кількості життів на екрані."""
         for heart_number in range(self.stats.ships_left):
-            # Для кожного життя, зміщуємо сердечко вправо
             offset_x = heart_number * (self.rect.width + 10)
             self.screen.blit(self.image, (self.rect.x + offset_x, self.rect.y))
