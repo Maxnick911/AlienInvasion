1f570d0f479bf209c8999cb08a8bd8b0e9c502b8
Maxnick911 <112699811+Maxnick911@users.noreply.github.com>
2024-08-13 17:52:10 +0200

    Update ship.py

--- ship.py
+++ ship.py
@@ -9,7 +9,6 @@ class Ship:
         self.image = pygame.image.load('images\\ship.png')
         self.rect = self.image.get_rect()
 
-        # Встановлюємо початкову позицію корабля
         self.rect.midbottom = self.screen_rect.midbottom
         self.x = float(self.rect.x)
 
@@ -17,21 +16,17 @@ class Ship:
         self.moving_left = False
 
     def update(self):
-        # Оновлюємо позицію корабля в залежності від напрямку руху
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
-        # Оновлюємо rect з плаваючої координати x
         self.rect.x = self.x
 
     def blitme(self):
-        # Відображення корабля на екрані
         self.screen.blit(self.image, self.rect)
 
     def center_ship(self):
-        # Використовуємо параметр з налаштувань для відступу
         offset = self.settings.ship_vertical_offset
         self.rect.midbottom = (self.screen_rect.midbottom[0], self.screen_rect.bottom - offset)
         self.x = float(self.rect.x)
