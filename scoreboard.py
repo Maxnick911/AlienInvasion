701cbcba2cd9aeed054946acf0bdf0a82bc3794c
Maxnick911 <112699811+Maxnick911@users.noreply.github.com>
2024-08-13 17:51:57 +0200

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -40,7 +40,7 @@ class Scoreboard:
         if self.stats.score > self.stats.high_score:
             self.stats.high_score = self.stats.score
             self.prep_high_score()
-            self.stats.save_high_score()  # Зберегти новий рекорд у файл
+            self.stats.save_high_score()
 
     def prep_level(self):
         level_str = str(self.stats.level)
